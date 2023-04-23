29b6b43a07e524a7e18c1f21b98319f51456f0b3
heba-saad <heba-saad@outlook.com>
2023-04-23 12:31:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that print aline using the function
+ * main - A program that print aline using the printf function
  * return: Always 0 (success)
  */
 int main(void)
