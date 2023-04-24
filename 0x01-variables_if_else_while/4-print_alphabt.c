925066007ccdda6eeb8488838c85b4fa0a07f129
heba-saad <heba-saad@outlook.com>
2023-04-24 16:34:34 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 97; i = 101; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
