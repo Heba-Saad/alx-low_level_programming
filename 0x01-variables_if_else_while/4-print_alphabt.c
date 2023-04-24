3cd178fbdd93faa4cde5eef10cb441a72e2d60e0
heba-saad <heba-saad@outlook.com>
2023-04-24 16:51:45 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int i;
-for (i = 97; i = 101; i++)
+for (i = 97; i <= 122; i++)
 {
 if (i != 101 && i != 113)
 {
@@ -16,5 +16,5 @@ putchar (i);
 }
 }
 putchar ('\n');
-return (0);
+return (0);
 }
