ac61a1711ce365b49d24e2eea56a380df571d6a3
heba-saad <heba-saad@outlook.com>
2023-05-25 10:51:48 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print - multiple choice print
+ * @x: char Type of print
+ * @T_func: funct
+ */
+typedef struct print
+{
+	char *x;
+	void (*T_func)(va_list);
+} t_print;
+
+
 int sum_tham_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
