cd95badd74e9d37981bf3c65b1af118b76862d02
Heba-Saad <heba-saad@outlook.com>
2023-06-12 12:13:16 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,35 @@
-#include "main.h"
+#include <main.h>
+
 /**
- * create_file - create a file
- * @filename: file name
- * @text_content: li text content
- * Return: 1 if sucess or -1 if an error ocurred
-*/
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
+ * Return: 1 on success, -1 on failure
+ */
+
 int create_file(const char *filename, char *text_content)
 {
-	int cr, wr;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
-	cr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (text_content == NULL)
+		text_content = "";
 
-	if (cr == -1)
-		return (-1);
 
-	if (!text_content)
-		return (1);
-
-	wr = write(cr, text_content, strlen(text_content));
-	if (wr == -1)
+	while (text_content[i] != '\0')
 	{
-		close(cr);
-		return (-1);
+		i++;
 	}
-	close(cr);
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
 	return (1);
 }
